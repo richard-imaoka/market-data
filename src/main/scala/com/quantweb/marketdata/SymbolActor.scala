92000f33e21001e19be32d2ae78536fc6328574e
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2014-12-05 01:09:26 +0900

    First SymbolActor tst case but with an Erasure compilar warning

--- src/main/scala/com/quantweb/marketdata/SymbolActor.scala
+++ src/main/scala/com/quantweb/marketdata/SymbolActor.scala
@@ -6,5 +6,11 @@ import akka.actor.Actor
  * Created by Richard S, Imaoka on 2014/11/30.
  */
 class SymbolActor extends Actor {
-    override def receive = Actor.emptyBehavior
+
+    var data: Map[String, Any] = Map[String, Any]()
+
+    override def receive = {
+        case receivedData: Map[String, Any] => data = receivedData
+    }
+
 }
--- src/test/scala/com/quantweb/marketdata/SymbolActorTest.scala
+++ src/test/scala/com/quantweb/marketdata/SymbolActorTest.scala
@@ -11,6 +11,16 @@ import org.scalatest.{Matchers, FlatSpecLike}
 class SymbolActorTest extends TestKit(ActorSystem("SymbolActorTest")) with FlatSpecLike with Matchers {
 
     "SymbolActor" should "update internal state on Market Data update" in {
+        val testActorRef = TestActorRef[SymbolActor](Props(new SymbolActor))
+        val data1 = Map[String, Any]("bestAsk" -> 101, "bestAskSize" -> 1050, "bestBid" -> 99, "bestBidSize" -> 980, "timeStamp" -> "2014-10-20 13:15:21:99")
+        val data2 = Map[String, Any]("bestAsk" -> 102, "bestAskSize" -> 1150, "bestBid" -> 99, "bestBidSize" -> 980, "timeStamp" -> "2014-10-20 13:15:24:99")
+
+        testActorRef ! data1
+        testActorRef.underlyingActor.data should be(data1)
+
+        testActorRef ! data2
+        testActorRef.underlyingActor.data should not be (data1)
+        testActorRef.underlyingActor.data should be(data2)
     }
 
     "SymbolActor" should "publish Market Data update" in {
