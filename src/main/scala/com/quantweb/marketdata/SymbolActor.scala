46c90d292abdadc11bbd4543500c947584aa9bac
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2014-12-06 02:17:42 +0900

    SymbolActor broadcast and its test

--- src/main/scala/com/quantweb/marketdata/SymbolActor.scala
+++ src/main/scala/com/quantweb/marketdata/SymbolActor.scala
@@ -1,6 +1,7 @@
 package com.quantweb.marketdata
 
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
+import akka.routing.{BroadcastRoutingLogic, Router}
 import com.quantweb.marketdata.SymbolActor.MarketDataUpdate
 
 /**
@@ -10,10 +11,24 @@ class SymbolActor extends Actor {
 
     var data: Map[String, Any] = Map[String, Any]()
 
+    /**
+     * SymbolActor always performs broadcast when receive() method receives MarketDataUpdate()
+     */
+    var router: Router = Router(BroadcastRoutingLogic())
+
     override def receive = {
-        case MarketDataUpdate(receivedData) => data = receivedData
+        case MarketDataUpdate(receivedData) => {
+            data = receivedData
+
+            if( router.routees.size > 0 ) //route with empty routee (= NoRoutee) will cause the message sent to DeadLetter offfice
+                router.route(data, self)
+        }
     }
 
+    /**
+     * register a new subscriber to this SymbolActor's routee
+     */
+    def registerSubscriber(subscriber: ActorRef) = router = router.addRoutee(subscriber)
 }
 
 object SymbolActor{
--- src/test/scala/com/quantweb/marketdata/SymbolActorTest.scala
+++ src/test/scala/com/quantweb/marketdata/SymbolActorTest.scala
@@ -1,16 +1,23 @@
 package com.quantweb.marketdata
 
-import akka.actor.{Props, ActorSystem}
-import akka.testkit.{TestActorRef, TestKit}
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import com.quantweb.marketdata.SymbolActor.MarketDataUpdate
-import org.scalatest.{Matchers, FlatSpecLike}
+import org.scalatest.{FlatSpecLike, Matchers}
+
+import scala.concurrent.duration.DurationInt
 
 /**
  * Created by Richar S. Imaoka on 2014/11/30.
  */
 
-class SymbolActorTest extends TestKit(ActorSystem("SymbolActorTest")) with FlatSpecLike with Matchers {
-
+class SymbolActorTest
+    extends TestKit(ActorSystem("SymbolActorTest"))
+    with FlatSpecLike
+    with Matchers       //To use "should" matchers
+    with ImplicitSender //To use "testActor" which is the entry point for various expectMsg assertions - http://doc.akka.io/docs/akka/snapshot/scala/testing.html
+                         //When mixing in the trait ImplicitSender this test actor is implicitly used as sender reference when dispatching messages from the test procedure.
+{
     "SymbolActor" should "update internal state on Market Data update" in {
         val testActorRef = TestActorRef[SymbolActor](Props(new SymbolActor))
         val data1 = Map[String, Any]("bestAsk" -> 101, "bestAskSize" -> 1050, "bestBid" -> 99, "bestBidSize" -> 980, "timeStamp" -> "2014-10-20 13:15:21:99")
@@ -24,13 +31,36 @@ class SymbolActorTest extends TestKit(ActorSystem("SymbolActorTest")) with FlatS
         testActorRef.underlyingActor.data should be(data2)
     }
 
-    "SymbolActor" should "publish Market Data update" in {
+    "SymbolActor" should "broadcast Market Data update" in {
+        val testActorRef = TestActorRef[SymbolActor](Props(new SymbolActor))
+
+        val subscriberActorRef1 = system.actorOf(Props(new PassThroughActor(testActor)))
+        val subscriberActorRef2 = system.actorOf(Props(new PassThroughActor(testActor)))
+        val subscriberActorRef3 = system.actorOf(Props(new PassThroughActor(testActor)))
+
+        testActorRef.underlyingActor.registerSubscriber(subscriberActorRef1)
+        testActorRef.underlyingActor.registerSubscriber(subscriberActorRef2)
+        testActorRef.underlyingActor.registerSubscriber(subscriberActorRef3)
+
+        val data = Map[String, Any]("bestAsk" -> 101, "bestAskSize" -> 1250, "bestBid" -> 99, "bestBidSize" -> 980, "timeStamp" -> "2014-10-20 13:15:21:99")
+
+        /**
+         * MarketDataUpdate sent to SymbolActor will be broadcast
+         */
+        testActorRef ! MarketDataUpdate(data)
+
+        /**
+         * Broadcast to 3 subscribers - the same message is received 3 times
+         */
+        expectMsg[Map[String, Any]](1.seconds, data)
+        expectMsg[Map[String, Any]](1.seconds, data)
+        expectMsg[Map[String, Any]](1.seconds, data)
     }
 
     "SymbolActor" should "update internal state on partial Market Data update" in {
     }
 
-    "SymbolActor" should "publish partial Market Data update" in {
+    "SymbolActor" should "broadcast partial Market Data update" in {
     }
 
     "SymbolActor" should "send entire data upon request" in {
